13df99b2514839ebb6b8a2e57ff578ae8c6838fd
Sven van de Scheur <svenvandescheur@gmail.com>
2024-01-12 13:29:37 +0100

    :memo: #4 - docs: update sample page story to include toolbar

--- src/components/page/page.stories.tsx
+++ src/components/page/page.stories.tsx
@@ -1,9 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 
+import { Button, ButtonLink } from "../button";
+import { Outline } from "../icon";
 import { Container, Grid } from "../layout";
 import { Column } from "../layout/column";
 import { Logo } from "../logo";
+import { Toolbar } from "../toolbar";
 import { Page } from "./page";
 
 type PagePropsAndCustomArgs = React.ComponentProps<typeof Page> & {
@@ -35,12 +38,40 @@ export const SamplePage: Story = {
     <Page>
       <Container debug={debug}>
         <Grid debug={debug}>
-          <Column debug={debug} span={12}>
+          <Column debug={debug} span={1}>
             <Logo
               href="/?path=/story/building-blocks-page--sample-page"
               hrefLabel="Navigate to story page"
               label="Maykin"
             />
+          </Column>
+
+          <Column debug={debug} span={11}>
+            <Toolbar align="end">
+              <Button variant="transparent">
+                <Outline.PencilIcon />
+                Zaaktypen
+              </Button>
+
+              <Button variant="transparent">
+                <Outline.ClipboardDocumentIcon />
+                Documenttypen
+              </Button>
+
+              <ButtonLink
+                href="https://www.example.com"
+                target="_blank"
+                variant="transparent"
+              >
+                <Outline.UserIcon />
+                Admin
+              </ButtonLink>
+
+              <Button variant="primary">
+                <Outline.ArrowRightStartOnRectangleIcon />
+                Uitloggen
+              </Button>
+            </Toolbar>
           </Column>
         </Grid>
       </Container>
