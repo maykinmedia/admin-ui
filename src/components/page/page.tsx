05f5570552127c33d7cdb8e164d6541eacccac5e
Sven van de Scheur <svenvandescheur@gmail.com>
2024-02-19 13:52:23 +0100

    :sparkles: - feat: add support for vertical alignment in page component

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -1,19 +1,27 @@
+import clsx from "clsx";
 import React from "react";
 
 import "./page.scss";
 
 export type PageProps = React.PropsWithChildren<{
-  // Props here.
+  /** Vertical alignment of page content. */
+  valign?: "start" | "middle";
 }>;
 
 /**
  * Provides the base theme for a page.
  * @param children
+ * @param valign
  * @param props
  * @constructor
  */
-export const Page: React.FC<PageProps> = ({ children, ...props }) => (
-  <div className="mykn-page" {...props}>
+export const Page: React.FC<PageProps> = ({ children, valign, ...props }) => (
+  <div
+    className={clsx("mykn-page", {
+      [`mykn-page--valign-${valign}`]: valign,
+    })}
+    {...props}
+  >
     {children}
   </div>
 );
