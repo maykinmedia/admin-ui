4bbe5bd8a69a1f095fdd1e629256e820d784c0f8
Sven van de Scheur <svenvandescheur@gmail.com>
2024-02-19 17:08:39 +0100

    :sparkles: - feat: add login template

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,6 @@ import "./style/default.scss";
 
 export * from "./components";
 export * as components from "./components";
+
+export * from "./templates";
+export * as templates from "./templates";
---
+++ src/templates/login/login.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Login } from "./login";
+
+const meta = {
+  title: "Templates/Login",
+  component: Login,
+  argTypes: { onSubmit: { action: "onSubmit" } },
+} satisfies Meta<typeof Login>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoginTemplate: Story = {
+  args: {
+    autoComplete: "off",
+    fields: [
+      {
+        label: "Gebruikersnaam",
+        name: "username",
+      },
+      {
+        label: "Wachtwoord",
+        name: "Password",
+      },
+    ],
+    labelSubmit: "Log in",
+    nonFieldErrors: ["Username or password invalid"],
+  },
+};
