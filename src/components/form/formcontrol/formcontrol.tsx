184691127f374e6455b90cdf9e2611ef6f14c8fa
Sven van de Scheur <svenvandescheur@gmail.com>
2024-02-27 14:01:43 +0100

    :sparkles: - feat: add support for direction prop in form component

--- src/components/form/formcontrol/formcontrol.tsx
+++ src/components/form/formcontrol/formcontrol.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import React, { useId } from "react";
 
 import { FormField, isInput, isSelect } from "../../../lib/form/typeguards";
@@ -7,16 +8,24 @@ import { Label } from "../label";
 import { Select } from "../select";
 import "./formcontrol.scss";
 
-export type FormControlProps = FormField & { error?: string };
+export type FormControlProps = FormField & {
+  /** The direction in which to render the form. */
+  direction?: "vertical" | "horizontal";
+
+  /** An error message to show. */
+  error?: string;
+};
 
 /**
  * Renders the correct form widget (based on its props shape) along with its
  * label and `error` message.
+ * @param direction
  * @param error
  * @param props
  * @constructor
  */
 export const FormControl: React.FC<FormControlProps> = ({
+  direction = "vertical",
   error = "",
   ...props
 }) => {
@@ -25,7 +34,12 @@ export const FormControl: React.FC<FormControlProps> = ({
   const idError = `${id}_error`;
 
   return (
-    <div className="mykn-form-control">
+    <div
+      className={clsx(
+        "mykn-form-control",
+        `mykn-form-control--direction-${direction}`,
+      )}
+    >
       {props.label && <Label htmlFor={_id}>{props.label}</Label>}
       <FormWidget
         id={_id}
