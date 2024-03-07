ec2a52a16b7fefd0e270860f9dd8bb7a48bc2c04
Sven van de Scheur <svenvandescheur@gmail.com>
2024-03-07 16:19:14 +0100

    :sparkles: - feat: add detail template

---
+++ src/templates/detail/detail.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Detail } from "./detail";
+
+const meta = {
+  title: "Templates/Detail",
+  component: Detail,
+  argTypes: { onSubmit: { action: "onSubmit" } },
+} satisfies Meta<typeof Detail>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DetailTemplate: Story = {
+  args: {
+    attributeGridProps: {
+      fieldsets: [
+        [
+          "Statussen",
+          {
+            fields: [
+              "uitgevoerd afgerond",
+              "voorstel voor besluitvorming opgesteld",
+            ],
+            span: 7,
+          },
+        ],
+        ["Rollen", { fields: ["klantcontacter", "behandelaar"], span: 5 }],
+        [
+          "Eigenschappen",
+          { fields: ["einde", "begin", "datum vergadering"], span: 7 },
+        ],
+        ["Resultaten", { fields: ["goedgekeurd", "afgekeurd"], span: 5 }],
+      ],
+      object: {
+        afgekeurd: "Goedgekeurd",
+        begin: "Erfpacht aanvraag wijzigen (2023-10-28)",
+        behandelaar: "Initiator",
+        "datum vergadering": "BInG aanvraag behandelen (2023-10-28)",
+        einde: "Erfpacht aanvraag wijzigen (2023-10-28)",
+        goedgekeurd: "Ingetrokken",
+        klantcontacter: "Beslisser",
+        "uitgevoerd afgerond": "Afgehandeld",
+        "voorstel voor besluitvorming opgesteld": "In behandeling genomen",
+      },
+    },
+  },
+};
