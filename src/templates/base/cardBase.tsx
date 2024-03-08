92545124f89fe8a05d5e8cf5941264d7fb4ab634
Sven van de Scheur <svenvandescheur@gmail.com>
2024-03-08 15:23:02 +0100

    :sparkles: - feat: allow templates to define actions

--- src/templates/base/cardBase.tsx
+++ src/templates/base/cardBase.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-import { Card, CardProps } from "../../components";
+import { Card, CardProps, Toolbar, ToolbarItem } from "../../components";
 import { Base, BaseProps } from "./base";
 
 export type CardBaseProps = BaseProps & {
+  actions?: ToolbarItem[];
+
   /** Card props.*/
   cardProps?: CardProps;
 };
@@ -13,12 +15,25 @@ export type CardBaseProps = BaseProps & {
  * @constructor
  */
 export const CardBase: React.FC<CardBaseProps> = ({
+  actions,
   children,
   cardProps,
   ...props
 }) => (
   <Base {...props}>
     <Card {...cardProps}>
+      {actions && (
+        <Toolbar
+          align="end"
+          pad={false}
+          variant="transparent"
+          items={actions.map((toolbarItem) => ({
+            pad: "h",
+            variant: "transparent",
+            ...toolbarItem,
+          }))}
+        ></Toolbar>
+      )}
       {children}
     </Card>
   </Base>
