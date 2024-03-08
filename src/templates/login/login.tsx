979421d43b688f2f336d74fcacc66478896b02e8
Sven van de Scheur <svenvandescheur@gmail.com>
2024-03-08 16:17:38 +0100

    :lipstick: - style: vertical align login page

--- src/templates/login/login.tsx
+++ src/templates/login/login.tsx
@@ -42,7 +42,12 @@ export const Login: React.FC<LoginProps> = ({
       });
 
   return (
-    <Base showHeader={false} columnProps={{ start: 5, span: 4 }} {...props}>
+    <Base
+      showHeader={false}
+      columnProps={{ start: 5, span: 4 }}
+      pageProps={{ valign: "middle" }}
+      {...props}
+    >
       <Card>
         <Body>
           {slotLogo || CustomLogo || <Logo />}
