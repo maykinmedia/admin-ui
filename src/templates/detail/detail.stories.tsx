36a8262b58599b37854df46eced61911b872d826
Sven van de Scheur <svenvandescheur@gmail.com>
2024-03-08 15:23:58 +0100

    :sparkles: - feat: update detail view with actions, inlines and sidebar

--- src/templates/detail/detail.stories.tsx
+++ src/templates/detail/detail.stories.tsx
@@ -13,36 +13,34 @@ type Story = StoryObj<typeof meta>;
 
 export const DetailTemplate: Story = {
   args: {
-    attributeGridProps: {
-      fieldsets: [
-        [
-          "Statussen",
-          {
-            fields: [
-              "uitgevoerd afgerond",
-              "voorstel voor besluitvorming opgesteld",
-            ],
-            span: 7,
-          },
-        ],
-        ["Rollen", { fields: ["klantcontacter", "behandelaar"], span: 5 }],
-        [
-          "Eigenschappen",
-          { fields: ["einde", "begin", "datum vergadering"], span: 7 },
-        ],
-        ["Resultaten", { fields: ["goedgekeurd", "afgekeurd"], span: 5 }],
+    fieldsets: [
+      [
+        "Statussen",
+        {
+          fields: [
+            "uitgevoerd afgerond",
+            "voorstel voor besluitvorming opgesteld",
+          ],
+          span: 7,
+        },
       ],
-      object: {
-        afgekeurd: "Goedgekeurd",
-        begin: "Erfpacht aanvraag wijzigen (2023-10-28)",
-        behandelaar: "Initiator",
-        "datum vergadering": "BInG aanvraag behandelen (2023-10-28)",
-        einde: "Erfpacht aanvraag wijzigen (2023-10-28)",
-        goedgekeurd: "Ingetrokken",
-        klantcontacter: "Beslisser",
-        "uitgevoerd afgerond": "Afgehandeld",
-        "voorstel voor besluitvorming opgesteld": "In behandeling genomen",
-      },
+      ["Rollen", { fields: ["klantcontacter", "behandelaar"], span: 5 }],
+      [
+        "Eigenschappen",
+        { fields: ["einde", "begin", "datum vergadering"], span: 7 },
+      ],
+      ["Resultaten", { fields: ["goedgekeurd", "afgekeurd"], span: 5 }],
+    ],
+    object: {
+      afgekeurd: "Goedgekeurd",
+      begin: "Erfpacht aanvraag wijzigen (2023-10-28)",
+      behandelaar: "Initiator",
+      "datum vergadering": "BInG aanvraag behandelen (2023-10-28)",
+      einde: "Erfpacht aanvraag wijzigen (2023-10-28)",
+      goedgekeurd: "Ingetrokken",
+      klantcontacter: "Beslisser",
+      "uitgevoerd afgerond": "Afgehandeld",
+      "voorstel voor besluitvorming opgesteld": "In behandeling genomen",
     },
   },
 };
--- src/templates/detail/detail.tsx
+++ src/templates/detail/detail.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-import { AttributeGrid, AttributeGridProps } from "../../components";
+import {
+  AttributeGrid,
+  AttributeGridProps,
+  Body,
+  DataGrid,
+  DataGridProps,
+  H2,
+  Toolbar,
+  ToolbarItem,
+} from "../../components";
+import { slugify } from "../../lib";
 import { BodyBase, BodyBaseProps } from "../base/bodyBase";
 
 export type DetailProps = BodyBaseProps & {
   attributeGridProps?: Partial<AttributeGridProps>;
   object: AttributeGridProps["object"];
   fieldsets: AttributeGridProps["fieldsets"];
+  showSidebar?: boolean;
+  inlines?: DataGridProps[];
 };
 
 /**
@@ -18,14 +30,61 @@ export const Detail: React.FC<DetailProps> = ({
   attributeGridProps,
   object,
   fieldsets,
+  showSidebar = true,
+  inlines = [],
   ...props
-}) => (
-  <BodyBase {...props}>
-    {children}
-    <AttributeGrid
-      {...attributeGridProps}
-      object={object}
-      fieldsets={fieldsets}
-    />
-  </BodyBase>
-);
+}) => {
+  const attributeGridSidebarItems: ToolbarItem[] = fieldsets
+    .filter((fieldset) => fieldset[0])
+    .map((fieldset) => ({
+      children: fieldset[0],
+      href: `#${slugify(fieldset[0] || "")}`,
+      pad: "h",
+      variant: "transparent",
+      textDecoration: "none",
+    }));
+
+  const inlinesSidebarItems: ToolbarItem[] = inlines
+    .filter((datagridProps) => datagridProps.title)
+    .map((datagridProps) => ({
+      children: datagridProps.title,
+      href: `#${slugify(datagridProps.title as string)}`,
+      pad: "h",
+      variant: "transparent",
+      textDecoration: "none",
+    }));
+
+  const sidebarItems = [...attributeGridSidebarItems, ...inlinesSidebarItems];
+
+  return (
+    <BodyBase
+      slotSidebar={
+        showSidebar && (
+          <Toolbar
+            direction="vertical"
+            items={sidebarItems}
+            sticky="top"
+            variant="transparent"
+          />
+        )
+      }
+      {...props}
+    >
+      {children}
+      <AttributeGrid
+        generateTitleIds={true}
+        {...attributeGridProps}
+        object={object}
+        fieldsets={fieldsets}
+      />
+      {inlines.map(({ title, ...props }, index) => {
+        return (
+          <Body key={title}>
+            <H2 id={title && slugify(title)}>{title}</H2>
+            <DataGrid key={index} {...props} />
+          </Body>
+        );
+      })}
+    </BodyBase>
+  );
+};
