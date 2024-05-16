c806df784f2116e97646d0d5200f9fdcdde7132e
Sven van de Scheur <svenvandescheur@gmail.com>
2024-05-16 14:45:44 +0200

    :recycle: - refactor: refactor ListTemplate

--- src/templates/list/list.tsx
+++ src/templates/list/list.tsx
@@ -4,111 +4,22 @@ import { DataGrid, DataGridProps, ToolbarItem } from "../../components";
 import { CardBase } from "../base";
 import { BodyBaseProps } from "../base/bodyBase";
 
-export type ListProps = BodyBaseProps & {
-  objectList: DataGridProps["objectList"];
-  fields?: DataGridProps["fields"];
-  count?: DataGridProps["count"];
-  dataGridProps?: DataGridProps;
-  editable?: DataGridProps["editable"];
-  loading?: DataGridProps["loading"];
-  pageSize?: DataGridProps["pageSize"];
-  pageSizeOptions?: DataGridProps["pageSizeOptions"];
-  selected?: DataGridProps["selected"];
+export type ListTemplateProps = BodyBaseProps & {
+  dataGridProps: DataGridProps;
   sidebarItems?: ToolbarItem[];
-  showPaginator?: DataGridProps["showPaginator"];
-  showSidebar?: boolean;
-  sort?: DataGridProps["sort"];
-  title?: React.ReactNode;
-  onChange?: DataGridProps["onChange"];
-  onClick?: DataGridProps["onClick"];
-  onEdit?: DataGridProps["onEdit"];
-  onSelect?: DataGridProps["onSelect"];
-  onSelectionChange?: DataGridProps["onSelectionChange"];
-  onPageChange?: DataGridProps["onPageChange"];
-  onPageSizeChange?: DataGridProps["onPageSizeChange"];
-} & ListSelectableConditional;
-
-type ListSelectableConditional =
-  | ListSelectableTrueProps
-  | ListSelectableFalseProps;
-
-type ListSelectableTrueProps = {
-  /** Whether checkboxes should be shown for every row. */
-  selectable: true;
-
-  /** The select item label. */
-  labelSelect: string;
-
-  /** The select all items label. */
-  labelSelectAll: string;
-};
-
-type ListSelectableFalseProps = {
-  /** Whether checkboxes should be shown for every row. */
-  selectable?: false;
-
-  /** The select item label. */
-  labelSelect?: string;
-
-  /** The select all items label. */
-  labelSelectAll?: string;
 };
 
 /**
  * List template
  * @constructor
  */
-export const List: React.FC<ListProps> = ({
+export const ListTemplate: React.FC<ListTemplateProps> = ({
   children,
   dataGridProps,
-  editable,
-  fields,
-  objectList,
-  count = objectList.length,
-  loading,
-  onChange,
-  onClick,
-  onEdit,
-  onPageChange,
-  onPageSizeChange,
-  onSelect,
-  onSelectionChange,
-  pageSize,
-  pageSizeOptions,
-  showPaginator,
-  selectable,
-  selected,
-  labelSelect,
-  labelSelectAll,
-  sort,
-  title,
   ...props
 }) => (
   <CardBase {...props}>
     {children}
-    <DataGrid
-      {...dataGridProps}
-      count={count}
-      editable={editable}
-      onEdit={onEdit}
-      fields={fields}
-      loading={loading}
-      objectList={objectList}
-      pageSize={pageSize}
-      pageSizeOptions={pageSizeOptions}
-      showPaginator={showPaginator}
-      selectable={selectable}
-      selected={selected}
-      title={title}
-      labelSelect={labelSelect}
-      labelSelectAll={labelSelectAll}
-      onSelect={onSelect}
-      onSelectionChange={onSelectionChange}
-      sort={sort}
-      onChange={onChange}
-      onClick={onClick}
-      onPageChange={onPageChange}
-      onPageSizeChange={onPageSizeChange}
-    />
+    <DataGrid {...dataGridProps} />
   </CardBase>
 );
