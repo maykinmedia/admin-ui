533f97a48bb46f292734b924d335faeff2e02c91
SilviaAmAm <silvia@maykinmedia.nl>
2024-05-24 16:13:32 +0200

    :sparkles: - feat: add stories for table with React nodes

--- src/components/data/attributetable/attributetable.stories.tsx
+++ src/components/data/attributetable/attributetable.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import * as React from "react";
 
+import { ButtonLink } from "../../button";
+import { Outline } from "../../icon";
 import { AttributeTable } from "./attributetable";
 
 const meta: Meta<typeof AttributeTable> = {
@@ -13,14 +16,42 @@ type Story = StoryObj<typeof meta>;
 export const AttributeTableComponent: Story = {
   args: {
     object: {
-      url: "https://www.example.com",
-      Omschrijving: "Afvalpas vervangen",
-      Zaaktype: "https://www.example.com",
-      Versie: 2,
-      Opmerkingen: null,
-      Actief: false,
-      Toekomstig: false,
-      Concept: true,
+      url: { label: "Url", value: "https://www.example.com" },
+      omschrijving: { label: "Omschrijving", value: "Afvalpas vervangen" },
+      zaaktype: { label: "Zaaktype", value: "https://www.example.com" },
+      versie: { label: "Versie", value: 2 },
+      opmerkingen: { label: "Opmerkingen", value: null },
+      actief: { label: "Actief", value: false },
+      toekomstig: { label: "Toekomstig", value: false },
+      concept: { label: "Concept", value: true },
+    },
+  },
+};
+
+export const AttributeTableComponentWithNodes: Story = {
+  args: {
+    object: {
+      button: {
+        label: "A button!",
+        value: (
+          <ButtonLink
+            href="https://www.example.com"
+            target="_blank"
+            variant="transparent"
+          >
+            <Outline.AcademicCapIcon />
+            Click me!
+          </ButtonLink>
+        ),
+      },
+      labelWithIcon: {
+        label: (
+          <div>
+            A label with icon <Outline.AcademicCapIcon />
+          </div>
+        ),
+        value: "Some value",
+      },
     },
   },
 };
