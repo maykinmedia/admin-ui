1a55cf773164f9eecd57cdfcb092a06a954b1ab6
Sven van de Scheur <svenvandescheur@gmail.com>
2024-09-02 16:56:53 +0200

    :sparkles: - feat: introduce containerType prop for container defining components

--- src/components/layout/page/page.tsx
+++ src/components/layout/page/page.tsx
@@ -4,6 +4,12 @@ import React from "react";
 import "./page.scss";
 
 export type PageProps = React.PropsWithChildren<{
+  /**
+   * Sets the "container-type" CSS attribute. Setting this to "normal" might fix
+   * problems regarding sizing of children.
+   */
+  containerType?: "inline-size" | "size" | "normal";
+
   /** Vertical alignment of page content. */
   valign?: "start" | "middle";
 
@@ -16,12 +22,14 @@ export type PageProps = React.PropsWithChildren<{
  */
 export const Page: React.FC<PageProps> = ({
   children,
+  containerType = "inline-size",
   pad = false,
   valign,
   ...props
 }) => (
   <div
     className={clsx("mykn-page", {
+      [`mykn-page--container-type-${containerType}`]: containerType,
       [`mykn-page--valign-${valign}`]: valign,
       "mykn-page--pad-h": pad === true || pad === "h",
       "mykn-page--pad-v": pad === true || pad === "v",
