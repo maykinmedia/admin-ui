4e82ee357410ec00e1c4ee225d14842b8e99c8da
Sven van de Scheur <svenvandescheur@gmail.com>
2024-10-17 16:22:07 +0200

    :memo: - docs: update DataGrid/ListTemplate stories to work with updated Storybook

--- src/components/data/datagrid/datagrid.stories.tsx
+++ src/components/data/datagrid/datagrid.stories.tsx
@@ -1,9 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import * as React from "react";
-import { useEffect, useState } from "react";
 
-import { SerializedFormData } from "../../../lib";
-import { AttributeData } from "../../../lib/data/attributedata";
+import { FIXTURE_PRODUCTS } from "../../../../.storybook/fixtures/products";
 import { Button } from "../../button";
 import { Page } from "../../layout";
 import { DataGrid } from "./datagrid";
@@ -26,8 +24,19 @@ const meta: Meta<typeof DataGrid> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+/**
+ * Typical example: a subset of `fields` from `objectList` representing one of
+ * multiple pages.
+ *
+ * `fields` prop accepts an array of either `string` or `TypedField` values,
+ * Please refer to `TypedField` for advanced usage.
+ */
 export const DataGridComponent = {
   args: {
+    title: "Products",
+    objectList: FIXTURE_PRODUCTS,
+    fields: ["name", "category", "price", "stock", "isAvailable"],
+    urlFields: ["url"],
     paginatorProps: {
       count: 100,
       page: 1,
@@ -40,94 +49,22 @@ export const DataGridComponent = {
         { label: 50 },
       ],
     },
-    objectList: [
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Afvalpas vervangen",
-        Zaaktype: "https://www.example.com",
-        Versie: 2,
-        Opmerkingen: null,
-        Actief: false,
-        Toekomstig: false,
-        Concept: true,
-      },
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Erfpacht wijzigen",
-        Zaaktype: "https://www.example.com",
-        Versie: 4,
-        Opmerkingen: null,
-        Actief: true,
-        Toekomstig: true,
-        Concept: true,
-      },
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Dakkapel vervangen",
-        Zaaktype: "https://www.example.com",
-        Versie: 1,
-        Opmerkingen: null,
-        Actief: false,
-        Toekomstig: false,
-        Concept: false,
-      },
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Dakkapel vervangen",
-        Zaaktype: "https://www.example.com",
-        Versie: 4,
-        Opmerkingen: null,
-        Actief: true,
-        Toekomstig: true,
-        Concept: true,
-      },
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Erfpacht wijzigen",
-        Zaaktype: "https://www.example.com",
-        Versie: 2,
-        Opmerkingen: null,
-        Actief: false,
-        Toekomstig: false,
-        Concept: true,
-      },
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Dakkapel vervangen",
-        Zaaktype: "https://www.example.com",
-        Versie: 4,
-        Opmerkingen: null,
-        Actief: true,
-        Toekomstig: true,
-        Concept: true,
-      },
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Erfpacht wijzigen",
-        Zaaktype: "https://www.example.com",
-        Versie: 1,
-        Opmerkingen: null,
-        Actief: false,
-        Toekomstig: false,
-        Concept: false,
-      },
-      {
-        url: "https://www.example.com",
-        Omschrijving: "Dakkapel vervangen",
-        Zaaktype: "https://www.example.com",
-        Versie: 1,
-        Opmerkingen: null,
-        Actief: false,
-        Toekomstig: false,
-        Concept: false,
-      },
-    ],
-    title: "Posts",
-    urlFields: ["url"],
   },
 };
 
-export const DecoratedDataGrid: Story = {
+/**
+ * Minimal example with only required props showing a simple table.
+ */
+export const MinimalExample = {
+  args: {
+    objectList: FIXTURE_PRODUCTS,
+  },
+};
+
+/**
+ * pass `true` to `decorate` prop to use value decoration (see `Value` component).
+ */
+export const Decorated: Story = {
   ...DataGridComponent,
   args: {
     ...DataGridComponent.args,
@@ -135,142 +72,84 @@ export const DecoratedDataGrid: Story = {
   },
 };
 
-export const SortableDataGrid: Story = {
+/**
+ * Pass `true` to `editable` prop to allow values to be edited, the exact fields
+ * to be editable can be controlled by passing `TypedField` items to the `fields`
+ * prop.
+ */
+export const EditableRows: Story = {
   ...DataGridComponent,
   args: {
     ...DataGridComponent.args,
-    sort: true,
+    fields: ["name", "category", "price", "stock", "isAvailable"],
+    editable: true,
+  },
+  argTypes: {
+    onEdit: { action: "onEdit" },
   },
 };
 
-export const SortedDataGrid: Story = {
+/**
+ * Pass `true` to `fieldsSelectable` prop to allow fields to be selectable, the
+ * default fields be active can be controlled by passing `TypedField` items to
+ * the `fields` prop.
+ */
+export const FieldsSelectable: Story = {
   ...DataGridComponent,
   args: {
     ...DataGridComponent.args,
-    sort: "Versie",
+    fields: [
+      { name: "id", type: "number", active: false },
+      "name",
+      "category",
+      "price",
+      "stock",
+      "isAvailable",
+      { name: "releaseDate", type: "date", active: false },
+    ],
+    fieldsSelectable: true,
+  },
+  argTypes: {
+    onFieldsChange: { action: "onFieldsChange" },
   },
 };
 
-export const JSONPlaceholderExample: Story = {
+/**
+ * Pass `true` to `filterable` prop to allow values to be filtered, the exact fields
+ * to be filterable can be controlled by passing `TypedField` items to the `fields`
+ * prop.
+ */
+export const Filterable: Story = {
+  ...DataGridComponent,
   args: {
-    objectList: [],
-    showPaginator: true,
-    sort: true,
-    title: "Posts",
+    ...DataGridComponent.args,
+    filterable: true,
+    fields: [
+      "name",
+      "category",
+      { name: "price", type: "number", filterable: false },
+      { name: "stock", type: "number", filterable: false },
+      { name: "isAvailable", type: "boolean" },
+    ],
   },
-  render: (args) => {
-    const [filterState, setFilterState] = useState<AttributeData>();
-    const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState<number>(args.paginatorProps?.page || 1);
-    const [pageSize, setPageSize] = useState<number>(args.pageSize || 10);
-    const [objectList, setObjectList] = useState<AttributeData[]>([]);
-    const [sort, setSort] = useState<string>("");
-
-    /**
-     * Fetches object from jsonplaceholder.typicode.com.
-     */
-    useEffect(() => {
-      setLoading(true);
-      const abortController = new AbortController();
-      const sortKey = sort.replace(/^-/, "");
-      const sortDirection = sort.startsWith("-") ? "desc" : "asc";
-
-      const filters = filterState
-        ? Object.fromEntries(
-            Object.entries(filterState)
-              .filter(([, value]) => value)
-              .map(([key, value]) => [key, String(value)]),
-          )
-        : {};
-      const searchParams = new URLSearchParams(filters);
-
-      // Process sorting and pagination locally in place for demonstration purposes.
-      fetch(
-        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${pageSize}&_sort=${sortKey}&_order=${sortDirection}&${searchParams}`,
-        {
-          signal: abortController.signal,
-        },
-      )
-        .then((response) => response.json())
-        .then((data: AttributeData[]) => {
-          setObjectList(data);
-          setLoading(false);
-        });
-
-      return () => {
-        abortController.abort();
-        setLoading(false);
-      };
-    }, [filterState, page, pageSize, sort]);
-
-    return (
-      <DataGrid
-        {...args}
-        count={100}
-        objectList={objectList}
-        loading={loading}
-        page={page}
-        pageSize={pageSize}
-        pageSizeOptions={[
-          { label: 10 },
-          { label: 20 },
-          { label: 30 },
-          { label: 40 },
-          { label: 50 },
-        ]}
-        selected={
-          // SelectableRows story
-          args.selected ||
-          (args.selectable
-            ? objectList.length > 0
-              ? [objectList[1], objectList[3], objectList[4]]
-              : undefined
-            : undefined)
-        }
-        equalityChecker={args.equalityChecker}
-        onPageChange={setPage}
-        onPageSizeChange={setPageSize}
-        onSort={(field) => setSort(field)}
-        onEdit={async (rowData: SerializedFormData) => {
-          setLoading(true);
-          await fetch(
-            `https://jsonplaceholder.typicode.com/posts/${rowData.id}`,
-            {
-              method: "PUT",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(rowData),
-            },
-          );
-          const index = objectList.findIndex(
-            (r) => Number(r.id) === rowData.id,
-          );
-          const newObjectList = [...objectList];
-          newObjectList[index] = rowData as AttributeData;
-          setObjectList(newObjectList);
-          setLoading(false);
-        }}
-        onFilter={(data) => {
-          setPage(1);
-          setFilterState(data);
-        }}
-      />
-    );
+  argTypes: {
+    onFilter: { action: "onFilter" },
   },
 };
 
-export const SelectableRows: Story = {
-  ...JSONPlaceholderExample,
+/**
+ * pass a `true` to `selectable` to allow selecting rows.
+ */
+export const Selectable: Story = {
+  ...DataGridComponent,
   args: {
-    ...JSONPlaceholderExample.args,
-    fields: ["userId", "id", "title"],
+    ...DataGridComponent.args,
     selectable: true,
     allowSelectAll: false,
     selectionActions: [
       {
-        children: "Aanmaken",
-        name: "create",
+        children: "Buy!",
+        name: "order",
         onClick: ({ detail }) => {
           // @ts-expect-error - TODO: type should be fixed
           alert(`${detail.length} items selected.`);
@@ -285,222 +164,155 @@ export const SelectableRows: Story = {
   },
 };
 
-export const SelectableRowsWithCustomMatchFunction: Story = {
-  ...JSONPlaceholderExample,
+/**
+ * Pass a function `(item1: AttributeData, item2: AttributeData) => bool`) to the `equalityChecker` prop to allow items
+ * in `selected` to be matched to item in `objectList`. If `equalityChecker` is omitted: strict checking
+ * (`item1 === item2`) is used to determine a match.
+ */
+export const SelectionMatchEqualityChecker: Story = {
+  ...Selectable,
   args: {
-    ...JSONPlaceholderExample.args,
-    fields: ["userId", "id", "title"],
+    ...Selectable.args,
+    selected: [
+      {
+        id: 1,
+      },
+      {
+        id: 5,
+      },
+    ],
+    equalityChecker: (item1, item2) => item1?.id === item2?.id,
+  },
+};
+
+/**
+ * pass `true` to `sort` prop to use sorting, sorting will be performed locally
+ * if no `onSort` prop is passed.
+ */
+export const Sortable: Story = {
+  ...DataGridComponent,
+  args: {
+    ...DataGridComponent.args,
+    sort: true,
+  },
+  // Don't set argTypes.onSort as it will result in a onSort prop being passed.
+};
+
+/**
+ * pass a `string` to `sort` (optionally prefix by `-` to invert sorting) prop
+ * to set a predefined order.
+ */
+export const Sorted: Story = {
+  ...Sortable,
+  args: {
+    ...Sortable.args,
+    sort: "-price",
+  },
+};
+
+export const FilterableSelectableSortable: Story = {
+  args: {
+    objectList: FIXTURE_PRODUCTS,
+    fields: [
+      "name",
+      "category",
+      { name: "price", type: "number", filterable: false },
+      { name: "stock", type: "number", filterable: false },
+      { name: "isAvailable", type: "boolean" },
+    ],
+    filterable: true,
     selectable: true,
+    selected: [
+      {
+        id: 1,
+      },
+      {
+        id: 5,
+      },
+    ],
+    equalityChecker: (item1, item2) => item1?.id === item2?.id,
+    allowSelectAll: true,
     selectionActions: [
       {
-        children: "Aanmaken",
-        name: "create",
+        children: "Buy!",
+        name: "order",
         onClick: ({ detail }) => {
-          // @ts-expect-error: TODO: type should be fixed
+          // @ts-expect-error - TODO: type should be fixed
           alert(`${detail.length} items selected.`);
         },
       },
     ],
-    selected: [
-      {
-        userId: 1,
-        id: 1,
-        title:
-          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-      },
-      {
-        userId: 1,
-        id: 5,
-        title: "nesciunt quas odio",
-        body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
-      },
-    ],
-    equalityChecker: (item1, item2) => item1.id === item2.id,
+    sort: "price",
   },
   argTypes: {
+    onFilter: { action: "onFilter" },
     onSelect: { action: "onSelect" },
     onSelectionChange: { action: "onSelectionChange" },
+    onSelectAllPages: { action: "onSelectAllPages" },
+    onSort: { action: "onSort" },
   },
 };
 
-export const EditableRows: Story = {
-  ...JSONPlaceholderExample,
+/**
+ * Pass `date` as `type` field on `TypedField` on filterable DataGrid to allow
+ * specifying a specific date.
+ */
+export const DateFilter: Story = {
+  ...Filterable,
   args: {
-    ...JSONPlaceholderExample.args,
-    // editable: true,
+    ...Filterable.args,
     fields: [
-      {
-        type: "number",
-        name: "userId",
-        options: [
-          { label: 1, value: 1 },
-          { label: 2, value: 2 },
-        ],
-        editable: true,
-      },
-      { name: "id", type: "number", editable: false },
-      "title",
-      {
-        type: "boolean",
-        name: "published",
-      },
+      "name",
+      "category",
+      "price",
+      "stock",
+      "isAvailable",
+      { name: "releaseDate", type: "date" },
     ],
   },
-  argTypes: {
-    onEdit: { action: "onEdit" },
-  },
-};
-
-export const FilterableRows: Story = {
-  ...JSONPlaceholderExample,
-  args: {
-    ...JSONPlaceholderExample.args,
-    filterable: true,
-    fields: [
-      {
-        type: "number",
-        name: "userId",
-        options: [
-          { label: 1, value: 1 },
-          { label: 2, value: 2 },
-        ],
-      },
-      { name: "id", type: "number", editable: false },
-      "title",
-      {
-        type: "boolean",
-        name: "published",
-      },
-    ],
-  },
-};
-
-export const FieldsSelectable: Story = {
-  ...JSONPlaceholderExample,
-  args: {
-    ...JSONPlaceholderExample.args,
-    filterable: true,
-    fields: [
-      {
-        type: "number",
-        name: "userId",
-        options: [
-          { label: 1, value: 1 },
-          { label: 2, value: 2 },
-        ],
-      },
-      { name: "id", type: "number", editable: false },
-      "title",
-      {
-        active: false,
-        type: "boolean",
-        name: "published",
-      },
-    ],
-    fieldsSelectable: true,
-  },
-  argTypes: {
-    onFieldsChange: { action: "onFieldsChange" },
-  },
-};
-
-export const DateRangeFilter: Story = {
   argTypes: {
     onFilter: {
       action: "onFilter",
     },
   },
+};
+
+/**
+ * Pass `daterange` as `type` field on `TypedField` on filterable DataGrid to
+ * allow specifying a start and end date.
+ */
+export const DateRangeFilter: Story = {
+  ...Filterable,
   args: {
+    ...Filterable.args,
     fields: [
-      {
-        name: "firstName",
-        type: "string",
-      },
-      {
-        name: "lastName",
-        type: "string",
-      },
-      {
-        name: "dateOfBirth",
-        type: "daterange",
-      },
+      "name",
+      "category",
+      "price",
+      "stock",
+      "isAvailable",
+      { name: "releaseDate", type: "daterange" },
     ],
-    filterable: true,
-    filterTransform: (data) => {
-      const { dateOfBirth, ..._data } = data;
-      const [dateOfBirth__gte = "", dateOfBirth__lte = ""] =
-        String(dateOfBirth).split("/");
-      return { dateOfBirth__gte, dateOfBirth__lte, ..._data };
+  },
+  argTypes: {
+    onFilter: {
+      action: "onFilter",
     },
-    objectList: [
-      {
-        firstName: "Albert",
-        lastName: "Einstein",
-        dateOfBirth: "1879-03-14",
-      },
-      {
-        firstName: "Marie",
-        lastName: "Curie",
-        dateOfBirth: "1867-11-07",
-      },
-      {
-        firstName: "Martin",
-        lastName: "Luther King Jr.",
-        dateOfBirth: "1929-01-15",
-      },
-      {
-        firstName: "Nelson",
-        lastName: "Mandela",
-        dateOfBirth: "1918-07-18",
-      },
-      {
-        firstName: "Isaac",
-        lastName: "Newton",
-        dateOfBirth: "1643-01-04",
-      },
-      {
-        firstName: "Mahatma",
-        lastName: "Gandhi",
-        dateOfBirth: "1869-10-02",
-      },
-      {
-        firstName: "Ada",
-        lastName: "Lovelace",
-        dateOfBirth: "1815-12-10",
-      },
-      {
-        firstName: "Leonardo",
-        lastName: "da Vinci",
-        dateOfBirth: "1452-04-15",
-      },
-      {
-        firstName: "Galileo",
-        lastName: "Galilei",
-        dateOfBirth: "1564-02-15",
-      },
-      {
-        firstName: "Charles",
-        lastName: "Darwin",
-        dateOfBirth: "1809-02-12",
-      },
-    ],
   },
 };
 
 export const JSXAsValue: Story = {
+  ...DataGridComponent,
   args: {
-    objectList: [
-      {
-        Id: 1,
-        Name: "Some product",
-        action: <Button variant="secondary">Buy!</Button>,
-      },
-      {
-        Id: 2,
-        Name: "Some other product",
-        action: <Button variant="secondary">Buy!</Button>,
-      },
-    ],
+    ...DataGridComponent.args,
+    fields: ["name", "category", "price", "stock", "isAvailable", "action"],
+    objectList: DataGridComponent.args.objectList.map((item) => ({
+      ...item,
+      action: (
+        <Button pad="h" variant="secondary">
+          Buy!
+        </Button>
+      ),
+    })),
   },
 };
