8c1c3047e7486af062c713f9bfa7194bb43d64b8
Sven van de Scheur <svenvandescheur@gmail.com>
2025-05-23 15:06:51 +0200

    :rotating_light: - fix: fix Sass deprecation warning in Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,37 +2,49 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 import * as path from "path";
 
 const config: StorybookConfig = {
-
-  stories: ["../docs/**/*.mdx", "../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  staticDirs: [{ from: "./static", to: "/static"}],
+  stories: [
+    "../docs/**/*.mdx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  staticDirs: [{ from: "./static", to: "/static" }],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-themes",
     "@storybook/addon-webpack5-compiler-swc",
-    "@chromatic-com/storybook"
+    "@chromatic-com/storybook",
   ],
   core: {
-    disableTelemetry: true
+    disableTelemetry: true,
   },
   framework: {
     name: "@storybook/react-webpack5",
     options: {
-      builder: {}
-    }
+      builder: {},
+    },
   },
   docs: {},
   webpackFinal: async (config) => {
     config?.module?.rules?.push({
       test: /\.scss$/,
-      use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../")
+      use: [
+        "style-loader",
+        "css-loader",
+        {
+          loader: "sass-loader",
+          options: {
+            api: "modern",
+          },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
     });
 
     // Add any other webpack config modifications here
 
     return config;
-  }
+  },
 };
 export default config;
