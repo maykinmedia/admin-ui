cc9a5d1772753362b098625f15d139ff8f69d084
Sven van de Scheur <svenvandescheur@gmail.com>
2025-06-13 10:06:29 +0200

    :memo: #210 - docs: improve Select stories

--- src/components/form/select/select.stories.tsx
+++ src/components/form/select/select.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryFn, StoryObj } from "@storybook/react-vite";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Formik } from "formik";
 import * as React from "react";
 import { action } from "storybook/actions";
@@ -9,15 +9,41 @@ import {
   PAGE_DECORATOR,
 } from "../../../../.storybook/decorators";
 import { Button } from "../../button";
+import { Toolbar } from "../../toolbar";
 import { Select } from "./select";
 
 const meta: Meta<typeof Select> = {
   title: "Form/Select",
   component: Select,
+  decorators: [PAGE_DECORATOR],
+  render: ({ name = "select", ...args }) => (
+    <Toolbar variant="transparent">
+      <Select name={name} variant="normal" {...args} />
+      <Select name={name + "-transparent"} variant="transparent" {...args} />
+    </Toolbar>
+  ),
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
+
+/**
+ * Assert `select`s native value.
+ * @param select
+ */
+async function assertNativeValue(select: HTMLSelectElement, value: string) {
+  await expect(select.value).toBe(value);
+}
+
+/**
+ * Assert `select`s serialized form value.
+ * @param select
+ */
+async function assertFormValue(select: HTMLSelectElement, value: string) {
+  const pre = await within(document.body).findByRole("log");
+  const data = JSON.parse(pre?.textContent || "{}");
+  await expect(data[select.name]).toBe(value);
+}
 
 export const SelectComponent: Story = {
   args: {
@@ -37,50 +63,129 @@ export const SelectComponent: Story = {
   decorators: [FORM_TEST_DECORATOR],
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const select = canvas.getByRole("combobox");
-    const clear = canvas.getByLabelText("Clear value");
+    const selects = canvas.getAllByRole("combobox");
 
-    const spy = fn();
-    select.addEventListener("change", spy);
-
-    await userEvent.click(select, { delay: 10 });
-    const junior = await canvas.findByText("Junior");
-    await userEvent.click(junior, { delay: 10 });
-
-    // Test that event listener on the (custom) select gets called.
-    await waitFor(testEventListener);
-
-    async function testEventListener() {
-      await expect(spy).toHaveBeenCalled();
-    }
-
-    // Test that the native select value attributes has the correct value.
-    await waitFor(testNativeSelectValue);
-
-    async function testNativeSelectValue() {
-      const form = await canvas.findByRole("form");
+    for (const select of selects) {
       const nativeSelect =
-        form.querySelector<HTMLSelectElement>("select[hidden]");
-      await expect(nativeSelect?.value).toBe("JR");
+        select.querySelector<HTMLSelectElement>("select[hidden]");
+      const clear = within(select).getByLabelText("Clear value");
+
+      const spy = fn();
+      select.addEventListener("change", spy);
+
+      await userEvent.click(select, { delay: 10 });
+      const junior = await within(select).findByText("Junior");
+      await userEvent.click(junior, { delay: 10 });
+
+      // Test that event listener on the (custom) select gets called.
+      await waitFor(expect(spy).toHaveBeenCalled);
+
+      // Test that the native select value attributes has the correct value.
+      await waitFor(() => assertNativeValue(nativeSelect!, "JR"));
+
+      // Test that the FormData serialization returns the correct value.
+      await waitFor(() => assertFormValue(nativeSelect!, "JR"));
+
+      await userEvent.click(clear, { delay: 10 });
     }
-
-    // Test that the FormData serialization returns the correct value.
-    await waitFor(testFormDataSerialization);
-
-    async function testFormDataSerialization() {
-      const pre = await canvas.findByRole("log");
-      const data = JSON.parse(pre?.textContent || "{}");
-      await expect(data.school_year).toBe("JR");
-    }
-
-    await userEvent.click(clear, { delay: 10 });
   },
 };
 
-export const TransparentSelect: Story = {
-  ...SelectComponent,
-  args: { ...SelectComponent.args, variant: "transparent" },
-  decorators: [...(SelectComponent.decorators as StoryFn[]), PAGE_DECORATOR],
+export const ValueBasedOnOptionValue: Story = {
+  args: {
+    options: [
+      { label: "Freshman", value: "FR" },
+      { label: "Sophomore", value: "SO" },
+      { label: "Junior", value: "JR" },
+      { label: "Senior", value: "SR" },
+      { label: "Graduate", value: "GR" },
+    ],
+    name: "school_year",
+    placeholder: "Select school year",
+    value: "JR",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const selects = canvas.getAllByRole("combobox");
+
+    for (const select of selects) {
+      const nativeSelect =
+        select.querySelector<HTMLSelectElement>("select[hidden]");
+      assertNativeValue(nativeSelect!, "JR");
+    }
+  },
+};
+
+export const ValueBasedOnOptionLabel: Story = {
+  args: {
+    options: [
+      { label: "Freshman" },
+      { label: "Sophomore" },
+      { label: "Junior" },
+      { label: "Senior" },
+      { label: "Graduate" },
+    ],
+    name: "school_year",
+    placeholder: "Select school year",
+    value: "Junior",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const selects = canvas.getAllByRole("combobox");
+
+    for (const select of selects) {
+      const nativeSelect =
+        select.querySelector<HTMLSelectElement>("select[hidden]");
+      assertNativeValue(nativeSelect!, "Junior");
+    }
+  },
+};
+
+export const Padding: Story = {
+  args: {
+    options: [
+      { label: "Normal padding" },
+      { label: "Horizontal padding" },
+      { label: "Vertical padding" },
+      { label: "No padding" },
+    ],
+    name: "school_year",
+    placeholder: "Select school year",
+    value: "Junior",
+  },
+  render: (args) => {
+    return (
+      <>
+        <Select {...args} pad={true} value="Normal padding" />
+        <br />
+        <br />
+        <Select {...args} pad="h" value="Horizontal padding" />
+        <br />
+        <br />
+        <Select {...args} pad="v" value="Vertical padding" />
+        <br />
+        <br />
+        <Select {...args} pad={false} value="No padding" />
+      </>
+    );
+  },
+};
+
+export const SelectMultiple: Story = {
+  args: {
+    options: [
+      { label: "Football", value: "football" },
+      { label: "Basketball", value: "basketball" },
+      { label: "Tennis", value: "tennis" },
+      { label: "Swimming", value: "swimming" },
+      { label: "Running", value: "running" },
+      { label: "Cycling", value: "cycling" },
+    ],
+    name: "favourite_sport",
+    placeholder: "Select favourite sport",
+    multiple: true,
+  },
+  decorators: [FORM_TEST_DECORATOR],
 };
 
 export const UsageWithFormik: Story = {
@@ -115,7 +220,7 @@ export const UsageWithFormik: Story = {
               onChange={(e) => handleChange(e)}
             ></Select>
             <pre role="log">{JSON.stringify(values)}</pre>
-            <Button type="submit">Verzenden</Button>;
+            <Button type="submit">Verzenden</Button>
           </form>
         )}
       </Formik>
@@ -141,96 +246,4 @@ export const UsageWithFormik: Story = {
 
     await userEvent.click(clear, { delay: 10 });
   },
-};
-
-export const ValueBasedOnOptionValue: Story = {
-  args: {
-    options: [
-      { label: "Freshman", value: "FR" },
-      { label: "Sophomore", value: "SO" },
-      { label: "Junior", value: "JR" },
-      { label: "Senior", value: "SR" },
-      { label: "Graduate", value: "GR" },
-    ],
-    name: "school_year",
-    placeholder: "Select school year",
-    value: "JR",
-  },
-};
-
-export const ValueBasedOnOptionLabel: Story = {
-  args: {
-    options: [
-      { label: "Freshman" },
-      { label: "Sophomore" },
-      { label: "Junior" },
-      { label: "Senior" },
-      { label: "Graduate" },
-    ],
-    name: "school_year",
-    placeholder: "Select school year",
-    value: "Junior",
-  },
-};
-
-export const Padding: Story = {
-  args: {
-    options: [
-      { label: "Normal padding" },
-      { label: "Horizontal padding" },
-      { label: "Vertical padding" },
-      { label: "No padding" },
-    ],
-    name: "school_year",
-    placeholder: "Select school year",
-    value: "Junior",
-  },
-  render: (args) => {
-    return (
-      <>
-        <Select {...args} pad={true} value="Normal padding" />
-        <br />
-        <br />
-        <Select {...args} pad="h" value="Horizontal padding" />
-        <br />
-        <br />
-        <Select {...args} pad="v" value="Vertical padding" />
-        <br />
-        <br />
-        <Select {...args} pad={false} value="No padding" />
-      </>
-    );
-  },
-};
-
-export const SelectSingle: Story = {
-  args: {
-    options: [
-      { label: "Freshman", value: "FR" },
-      { label: "Sophomore", value: "SO" },
-      { label: "Junior", value: "JR" },
-      { label: "Senior", value: "SR" },
-      { label: "Graduate", value: "GR" },
-    ],
-    name: "school_year",
-    placeholder: "Select school year",
-  },
-  decorators: [FORM_TEST_DECORATOR],
-};
-
-export const SelectMultiple: Story = {
-  args: {
-    options: [
-      { label: "Football", value: "football" },
-      { label: "Basketball", value: "basketball" },
-      { label: "Tennis", value: "tennis" },
-      { label: "Swimming", value: "swimming" },
-      { label: "Running", value: "running" },
-      { label: "Cycling", value: "cycling" },
-    ],
-    name: "favourite_sport",
-    placeholder: "Select favourite sport",
-    multiple: true,
-  },
-  decorators: [FORM_TEST_DECORATOR],
 };
