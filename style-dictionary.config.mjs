2a4fb803bceca724d732b01bbadce840ed432948
Julian Roeland <julianroeland@gmail.com>
2025-09-10 18:04:24 +0200

    :sparkles: - feat: initial bootstrap of design token refactor

--- style-dictionary.config.mjs
+++ style-dictionary.config.mjs
@@ -1,17 +1,15 @@
 import StyleDictionary from "style-dictionary";
 
-
 StyleDictionary.registerTransform({
   name: "attribute/font",
   type: "attribute",
-  transform: prop =>
-    ({
-      category: prop.path[0],
-      type: prop.path[1],
-      family: prop.path[2],
-      weight: prop.path[3],
-      style: prop.path[4]
-    })
+  transform: (prop) => ({
+    category: prop.path[0],
+    type: prop.path[1],
+    family: prop.path[2],
+    weight: prop.path[3],
+    style: prop.path[4],
+  }),
 });
 
 StyleDictionary.registerFormat({
@@ -21,61 +19,65 @@ StyleDictionary.registerFormat({
 
     // https://developer.mozilla.org/en-US/docs/Web/CSS/@font-face/src
     const formatsMap = {
-      "woff2": "woff2",
-      "woff": "woff",
-      "ttf": "truetype",
-      "otf": "opentype",
-      "svg": "svg",
-      "eot": "embedded-opentype"
+      woff2: "woff2",
+      woff: "woff",
+      ttf: "truetype",
+      otf: "opentype",
+      svg: "svg",
+      eot: "embedded-opentype",
     };
 
-    return allTokens.reduce((fontList, prop) => {
-      const {
-        attributes: { family, weight, style },
-        formats,
-        value: path
-      } = prop;
+    return allTokens
+      .reduce((fontList, prop) => {
+        const {
+          attributes: { family, weight, style },
+          formats,
+          value: path,
+        } = prop;
 
-      const urls = formats
-        .map(extension => `url("${fontPathPrefix}${path}.${extension}") format("${formatsMap[extension]}")`);
+        const urls = formats.map(
+          (extension) =>
+            `url("${fontPathPrefix}${path}.${extension}") format("${formatsMap[extension]}")`,
+        );
 
-      const fontCss = [
-        "@font-face {",
-        `\n\tfont-family: "${family}";`,
-        `\n\tfont-style: ${style};`,
-        `\n\tfont-weight: ${weight};`,
-        `\n\tsrc: ${urls.join(",\n\t\t\t ")};`,
-        "\n\tfont-display: fallback;",
-        "\n}\n"
-      ].join("");
+        const fontCss = [
+          "@font-face {",
+          `\n\tfont-family: "${family}";`,
+          `\n\tfont-style: ${style};`,
+          `\n\tfont-weight: ${weight};`,
+          `\n\tsrc: ${urls.join(",\n\t\t\t ")};`,
+          "\n\tfont-display: fallback;",
+          "\n}\n",
+        ].join("");
 
-      fontList.push(fontCss);
+        fontList.push(fontCss);
 
-      return fontList;
-    }, []).join("\n");
-  }
+        return fontList;
+      }, [])
+      .join("\n");
+  },
 });
 
+const isFont = (token) => token.type === "font";
+
 export default {
-  source: [
-    "src/design-tokens/tokens/*.json"
-  ],
+  source: ["src/design-tokens/tokens/**/*.json"],
   platforms: {
     "css/properties": {
-      transforms: ["attribute/color", "color/hsl-4", "name/kebab"],
+      transforms: ["attribute/color", "name/kebab"],
       buildPath: "src/design-tokens/compiled/",
       files: [
         {
           destination: "properties.css",
-          filter: (token) => token.type !== "font",
+          filter: !isFont,
           format: "css/variables",
           options: {
             outputReferences(token) {
               return token.attributes.outputReferences !== false;
-            }
-          }
-        }
-      ]
+            },
+          },
+        },
+      ],
     },
     "css/fonts": {
       transforms: ["attribute/color", "attribute/font", "name/kebab"],
@@ -83,23 +85,28 @@ export default {
       files: [
         {
           destination: "fonts.css",
-          filter: (token) => token.type === "font",
+          filter: isFont,
           format: "font-face",
           options: {
-            fontPathPrefix: "/src/fonts/"
-          }
-        }
-      ]
+            fontPathPrefix: "/src/fonts/",
+          },
+        },
+      ],
     },
-    "tokens": {
-      transforms: ["attribute/color", "attribute/cti", "color/hsl-4", "name/kebab"],
+    tokens: {
+      transforms: [
+        "attribute/color",
+        "attribute/cti",
+        "color/hsl-4",
+        "name/kebab",
+      ],
       buildPath: "src/design-tokens/compiled/",
       files: [
         {
           destination: "tokens.json",
           format: "json",
-        }
-      ]
+        },
+      ],
     },
-  }
+  },
 };
