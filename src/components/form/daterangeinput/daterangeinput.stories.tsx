0f0f4e280aec665ac5123fc3dd585052ae7ac68f
Sven van de Scheur <svenvandescheur@gmail.com>
2025-10-20 17:09:00 +0200

    :bug: - fix: fix issue where React could use stale data due to use as index as key

--- src/components/form/daterangeinput/daterangeinput.stories.tsx
+++ src/components/form/daterangeinput/daterangeinput.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { expect, userEvent, waitFor, within } from "storybook/test";
+import { expect, userEvent, within } from "storybook/test";
 
 import { FORM_TEST_DECORATOR } from "../../../../.storybook/decorators";
 import { DateRangeInput } from "./daterangeinput";
@@ -59,30 +59,6 @@ export const SeparatedInputs: Story = {
   },
 };
 
-export const ContinuousTyping: Story = {
-  args: {
-    name: "date",
-    format: "DDMMYYYY",
-  },
-  decorators: [FORM_TEST_DECORATOR],
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const log = canvas.getByRole("log");
-
-    const inputStart = await canvas.getAllByRole("textbox")[0];
-    await userEvent.type(inputStart, "02081988", { delay: 60 });
-    await userEvent.tab({ delay: 60 });
-
-    const inputEnd = await canvas.getAllByRole("textbox")[3];
-    await userEvent.type(inputEnd, "15092023", { delay: 60 });
-    await userEvent.tab({ delay: 60 });
-
-    await expect(JSON.parse(log.textContent || "{}").date).toBe(
-      "1988-08-02/2023-09-15",
-    );
-  },
-};
-
 export const NormalizeStartEnd: Story = {
   args: {
     name: "date",
@@ -93,62 +69,20 @@ export const NormalizeStartEnd: Story = {
     const canvas = within(canvasElement);
     const log = canvas.getByRole("log");
 
-    const inputStart = await canvas.getAllByRole("textbox")[0];
-    await userEvent.type(inputStart, "15092023", { delay: 60 });
-    await userEvent.tab({ delay: 60 });
+    const inputs = await canvas.getAllByRole("textbox");
+    await userEvent.type(inputs[0], "15", { delay: 60 });
+    await userEvent.type(inputs[1], "09", { delay: 60 });
+    await userEvent.type(inputs[2], "2023", { delay: 60 });
 
-    const inputEnd = await canvas.getAllByRole("textbox")[3];
-    await userEvent.type(inputEnd, "02081988", { delay: 60 });
-    await userEvent.tab({ delay: 60 });
+    await userEvent.type(inputs[3], "02", { delay: 60 });
+    await userEvent.type(inputs[4], "08", { delay: 60 });
+    await userEvent.type(inputs[5], "1988", { delay: 60 });
+
+    await userEvent.tab();
 
     await expect(JSON.parse(log.textContent || "{}").date).toBe(
       "1988-08-02/2023-09-15",
     );
-  },
-};
-
-export const ClearSections: Story = {
-  args: {
-    name: "date",
-    format: "DDMMYYYY",
-    value: "1988-08-02/2023-09-15",
-  },
-  decorators: [FORM_TEST_DECORATOR],
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const log = canvas.getByRole("log");
-
-    const dayEnd = await canvas.getAllByRole("textbox")[3];
-    const yearEnd = await canvas.getAllByRole("textbox")[5];
-
-    await userEvent.click(yearEnd, { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-
-    await waitFor(() =>
-      expect(document.activeElement?.getAttribute("placeholder")).toBe(
-        dayEnd.getAttribute("placeholder"),
-      ),
-    );
-
-    const dayStart = await canvas.getAllByRole("textbox")[0];
-    const yearStart = await canvas.getAllByRole("textbox")[2];
-
-    await userEvent.click(yearStart, { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-
-    await waitFor(() =>
-      expect(document.activeElement?.getAttribute("placeholder")).toBe(
-        dayStart.getAttribute("placeholder"),
-      ),
-    );
-
-    await userEvent.click(yearEnd);
-    await userEvent.tab({ delay: 60 });
-    await expect(JSON.parse(log.textContent || "{}").date).toBe("");
   },
 };
 
@@ -162,13 +96,16 @@ export const IsoFormat: Story = {
     const canvas = within(canvasElement);
     const log = canvas.getByRole("log");
 
-    const inputStart = await canvas.getAllByRole("textbox")[0];
-    await userEvent.type(inputStart, "19880802", { delay: 60 });
-    await userEvent.tab({ delay: 60 });
+    const inputs = await canvas.getAllByRole("textbox");
+    await userEvent.type(inputs[0], "1988", { delay: 60 });
+    await userEvent.type(inputs[1], "08", { delay: 60 });
+    await userEvent.type(inputs[2], "02", { delay: 60 });
 
-    const inputEnd = await canvas.getAllByRole("textbox")[3];
-    await userEvent.type(inputEnd, "20230915", { delay: 60 });
-    await userEvent.tab({ delay: 60 });
+    await userEvent.type(inputs[3], "2023", { delay: 60 });
+    await userEvent.type(inputs[4], "09", { delay: 60 });
+    await userEvent.type(inputs[5], "15", { delay: 60 });
+
+    await userEvent.tab();
 
     await expect(JSON.parse(log.textContent || "{}").date).toBe(
       "1988-08-02/2023-09-15",
