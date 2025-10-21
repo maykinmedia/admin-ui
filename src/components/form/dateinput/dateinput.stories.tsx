d92f4a34e62cfe468963aff3e1db08e51d131a5d
Sven van de Scheur <svenvandescheur@gmail.com>
2025-10-21 12:09:50 +0200

    :recycle: #255 - refactor: remove dateinput autofocus for now

--- src/components/form/dateinput/dateinput.stories.tsx
+++ src/components/form/dateinput/dateinput.stories.tsx
@@ -41,49 +41,6 @@ export const SeparatedInputs: Story = {
   },
 };
 
-export const ContinuousTyping: Story = {
-  args: {
-    name: "date",
-    format: "DDMMYYYY",
-  },
-  decorators: [FORM_TEST_DECORATOR],
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const log = canvas.getByRole("log");
-
-    const input = await canvas.getAllByRole("textbox")[0];
-
-    await userEvent.type(input, "15092023", { delay: 60 });
-    await expect(JSON.parse(log.textContent || "{}").date).toBe("2023-09-15");
-  },
-};
-
-export const ClearSections: Story = {
-  args: {
-    name: "date",
-    format: "DDMMYYYY",
-    value: "2023-09-15",
-  },
-  decorators: [FORM_TEST_DECORATOR],
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const day = await canvas.getAllByRole("textbox")[0];
-    const year = await canvas.getAllByRole("textbox")[2];
-    const log = canvas.getByRole("log");
-
-    await userEvent.click(year, { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-    await userEvent.keyboard("{Backspace}", { delay: 60 });
-
-    await expect(document.activeElement?.getAttribute("placeholder")).toBe(
-      day.getAttribute("placeholder"),
-    );
-    await userEvent.type(day, "02081988", { delay: 60 });
-    await expect(JSON.parse(log.textContent || "{}").date).toBe("1988-08-02");
-  },
-};
-
 export const IsoFormat: Story = {
   args: {
     name: "date",
@@ -94,9 +51,11 @@ export const IsoFormat: Story = {
     const canvas = within(canvasElement);
     const log = canvas.getByRole("log");
 
-    const input = await canvas.getAllByRole("textbox")[0];
+    const inputs = await canvas.getAllByRole("textbox");
 
-    await userEvent.type(input, "20230915", { delay: 60 });
+    await userEvent.type(inputs[0], "2023", { delay: 60 });
+    await userEvent.type(inputs[1], "09", { delay: 60 });
+    await userEvent.type(inputs[2], "15", { delay: 60 });
     await expect(JSON.parse(log.textContent || "{}").date).toBe("2023-09-15");
   },
 };
