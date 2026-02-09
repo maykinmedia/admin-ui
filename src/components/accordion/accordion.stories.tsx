c2e6285ca282c0c9fe088642035380d128060285
robinvandermolen <robin@maykinmedia.nl>
2026-02-09 14:12:04 +0100

    :sparkles: open-formulieren/admin-ui#7 - feat: Control accordion opened state using props
    Adding 'open', 'onOpen' and 'onClose' props to the accordion component. Using a similar setup as the dropdown and modal components, the 'open' property updates the Accordion internal 'open' state using a useEffect.

--- src/components/accordion/accordion.stories.tsx
+++ src/components/accordion/accordion.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { expect, fn, userEvent, waitFor, within } from "storybook/test";
 
 import { Accordion } from "./accordion";
 
@@ -12,16 +13,108 @@ type Story = StoryObj<typeof meta>;
 
 export const Accordions: Story = {
   args: {
+    variant: "primary",
     children: "Open accordion",
     items: [
       {
         componentType: "button",
-        children: "Button text",
+        children: "Accordion item 1",
       },
       {
         componentType: "button",
-        children: "Button text",
+        children: "Accordion item 2",
       },
     ],
+    onOpen: fn(),
+    onClose: fn(),
+  },
+  play: async ({ canvasElement, step, args }) => {
+    const canvas = within(canvasElement);
+    const accordionButton = canvas.getByRole("button", {
+      name: "Open accordion",
+    });
+
+    await step("Accordion is initially closed", () => {
+      // Accordion is initially closed
+      expect(accordionButton).toBeVisible();
+      expect(accordionButton).toHaveAttribute("aria-expanded", "false");
+
+      // Expect the accordion items to be visible
+      expect(
+        canvas.queryByRole("button", { name: "Accordion item 1" }),
+      ).not.toBeInTheDocument();
+      expect(
+        canvas.queryByRole("button", { name: "Accordion item 2" }),
+      ).not.toBeInTheDocument();
+    });
+
+    await step("Opening accordion button", async () => {
+      // Open the accordion
+      await userEvent.click(accordionButton);
+      expect(args.onOpen).toBeCalled();
+      expect(accordionButton).toHaveAttribute("aria-expanded", "true");
+
+      // Expect the accordion items to be visible
+      expect(
+        canvas.getByRole("button", { name: "Accordion item 1" }),
+      ).toBeVisible();
+      expect(
+        canvas.getByRole("button", { name: "Accordion item 2" }),
+      ).toBeVisible();
+    });
+
+    await step("Closing accordion", async () => {
+      // Closing the accordion
+      await userEvent.click(accordionButton);
+      expect(args.onClose).toBeCalled();
+      expect(accordionButton).toHaveAttribute("aria-expanded", "false");
+
+      // Expect the accordion items to be hidden
+      expect(
+        canvas.queryByRole("button", { name: "Accordion item 1" }),
+      ).not.toBeInTheDocument();
+      expect(
+        canvas.queryByRole("button", { name: "Accordion item 2" }),
+      ).not.toBeInTheDocument();
+    });
+  },
+};
+
+export const AccordionInitiallyOpened: Story = {
+  args: {
+    variant: "primary",
+    children: "Open accordion",
+    items: [
+      {
+        componentType: "button",
+        children: "Accordion item 1",
+      },
+      {
+        componentType: "button",
+        children: "Accordion item 2",
+      },
+    ],
+    open: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const accordionButton = canvas.getByRole("button", {
+      name: "Open accordion",
+    });
+
+    // Expect the accordion button to be visible and have to aria-expanded attribute.
+    expect(accordionButton).toBeVisible();
+    // Wait for the component state to be updated
+    await waitFor(() =>
+      expect(accordionButton).toHaveAttribute("aria-expanded", "true"),
+    );
+
+    // Expect the accordion items to be visible
+    expect(
+      canvas.getByRole("button", { name: "Accordion item 1" }),
+    ).toBeVisible();
+    expect(
+      canvas.getByRole("button", { name: "Accordion item 2" }),
+    ).toBeVisible();
   },
 };
